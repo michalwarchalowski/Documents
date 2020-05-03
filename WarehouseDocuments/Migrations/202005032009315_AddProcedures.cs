6a9a920fcd7ec0c59eff029283fc53588bc814d9
Michał Warchałowski <michalwarchalowski@gmail.com>
2020-05-03 22:32:36 +0200

    add procedures

---
+++ WarehouseDocuments/Migrations/202005032009315_AddProcedures.cs
@@ -0,0 +1,20 @@
+﻿namespace WarehouseDocuments.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    using System.IO;
+
+    public partial class AddProcedures : DbMigration
+    {
+        public override void Up()
+        {
+            var path = "../../Scripts/Procedures.sql";
+            string readText = File.ReadAllText(path);
+            Sql(readText);   
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
